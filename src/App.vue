26418f9642cf503236ab764c511b187f5efd50ed
nikolina777 <152617209+nikolina777@users.noreply.github.com>
2023-12-01 19:54:36 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <nav id="nav" class="navbar navbar-light navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/logo_rukovnik.png" alt="Logo" height="80" class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/signup" class="nav-link">Signup</router-link>
+            </li>
+          </ul>
+          <form class="d-flex ms-auto" role="search">
+            <input class="form-control me-2" type="search" placeholder="Pretraga" aria-label="Search" />
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+  <div class="col-2"></div>
+  <div class="col-7">
+    <NoteCard/>
+    <NoteCard/>
+  </div>
+  <div class="col-3">
+  ... ovdje ide desni stupac!
+  </div>
+  </div>
+ </template>
+
+<script>
+// @ is an alias to /src
+import NoteCard from '@/components/NoteCard.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NoteCard,
+  }
+}
+</script>
