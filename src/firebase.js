c11691666371c4b7092e82a60270f3297db3692c
nikolina777 <152617209+nikolina777@users.noreply.github.com>
2023-12-03 23:39:20 +0100

    import firebase modula

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-import firebase from 'firebase/app';
-import firebase from 'firebase/auth';
-import firebase from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3HBbbC2y9OFncuqKzRlAYO4J1lHl7Dm8",
-    authDomain: "rukovnik-cdc5a.firebaseapp.com",
-    projectId: "rukovnik-cdc5a",
-    storageBucket: "rukovnik-cdc5a.appspot.com",
-    messagingSenderId: "72936066238",
-    appId: "1:72936066238:web:9ca8b708373a486218a2ef"
-  };
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyC3HBbbC2y9OFncuqKzRlAYO4J1lHl7Dm8",
+  authDomain: "rukovnik-cdc5a.firebaseapp.com",
+  projectId: "rukovnik-cdc5a",
+  storageBucket: "rukovnik-cdc5a.appspot.com",
+  messagingSenderId: "72936066238",
+  appId: "1:72936066238:web:9ca8b708373a486218a2ef"
+};
 
-  export default {
-    firebase,
-  }
+const app = initializeApp(firebaseConfig);
+
+export const auth = app.auth();
+export const firestore = app.firestore();
+export default app;
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,34 +1,61 @@
 <template>
     <div class="Signup">
-    <h1>This is a Signup page</h1>
-    <div class="container">
-    <div class="row">
-    <div class="col-sm"></div>
-    <div class="col-sm">
-    <form>
-    <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control"
-   id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter
-   email" />
-    <small id="emailHelp" class="form-text text-muted" >We'll
-   never share your email with anyone else.</small>
+      <h1>This is a Signup page</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm"></div>
+          <div class="col-sm">
+            <form @submit.prevent="signup">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" v-model="username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" v-model="password" id="exampleInputPassword1" placeholder="Password" />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword2">Repeat Password</label>
+                <input type="password" class="form-control" v-model="passwordRepeat" id="exampleInputPassword2" placeholder="Repeat password" />
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+          <div class="col-sm"></div>
+        </div>
+      </div>
     </div>
-    <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control"
-   id="exampleInputPassword1" placeholder="Password" />
-    </div>
-    <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control"
-   id="exampleInputPassword1" placeholder="Repeat password" />
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    </div>
-    <div class="col-sm"></div>
-    </div>
-    </div>
-    </div>
-   </template>
+  </template>
+  
+  <script>
+  import { auth } from '@/firebase';
+  
+  export default {
+    name: 'Signup',
+    data() {
+      return {
+        username: '',
+        password: '',
+        passwordRepeat: '',
+      };
+    },
+    methods: {
+      signup() {
+        if (this.password !== this.passwordRepeat) {
+          console.error("Lozinke se ne podudaraju");
+          return;
+        }
+  
+        auth.createUserWithEmailAndPassword(this.username, this.password)
+          .then(() => {
+            console.log('Uspješna registracija');
+          })
+          .catch((error) => {
+            console.error("Došlo je do pogreške", error);
+          });
+      },
+    },
+  };
+  </script>
+  
