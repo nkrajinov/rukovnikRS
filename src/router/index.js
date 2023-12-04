031138f8d6a40bdd8a9fbdee5ecf8620e7560c87
nikolina777 <152617209+nikolina777@users.noreply.github.com>
2023-12-04 23:20:08 +0100

    router

--- src/router/index.js
+++ src/router/index.js
@@ -10,18 +10,12 @@ const routes = [
   {
     path: '/Login',
     name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/signup',
     name: 'Signup',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+    component: () => import('../views/Signup.vue')
   }
 ]
 
