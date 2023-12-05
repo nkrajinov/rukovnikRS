53f5df1824261d62407c46b54937d053c068f40c
nikolina777 <152617209+nikolina777@users.noreply.github.com>
2023-12-05 23:18:40 +0100

    login scripta

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,50 @@
 <template>
   <div class="about">
-  <h1>This is an login page</h1>
-  <div class="container">
-  <div class="row">
-  <div class="col-sm"></div>
-  <div class="col-sm">
-  <form>
-  <div class="form-group">
-  <label for="exampleInputEmail1">Email address</label>
-  <input type="email" class="form-control"
- id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter
- email" />
-  <small id="emailHelp" class="form-text text-muted" >We'll
- never share your email with anyone else.</small>
+    <h1>This is an login page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" v-model="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" class="form-text text-muted" >We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <button type="button" @click="login()" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
   </div>
-  <div class="form-group">
-  <label for="exampleInputPassword1">Password</label>
-  <input type="password" class="form-control"
- id="exampleInputPassword1" placeholder="Password" />
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-  </div>
-  <div class="col-sm"></div>
-  </div>
-  </div>
-  </div>
- </template>
+</template>
+<script>
+import { firebase } from 'firebase/app';
+export default {
+  name: "login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("login..." + this.username)
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.username, this.password)
+        .then(function(result) {
+          console.log("Uspjesna prijava", result);
+        }).catch(function(error) {
+          console.error('Greska: ', error);
+        });
+    },
+  },
+}
+</script>
