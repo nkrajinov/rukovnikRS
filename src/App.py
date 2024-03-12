616a377a86d9dedf5a8a127f57ed2d93d5d2250b
nikolina777 <152617209+nikolina777@users.noreply.github.com>
2024-03-12 22:43:38 +0100

    modul flask_login

---
+++ src/App.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Zamijenite 'your_secret_key' sa svojim tajnim ključem
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask-Login!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
