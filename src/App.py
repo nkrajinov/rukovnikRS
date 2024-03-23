0b0a5abe519e71bab64bf8aefb57ff542ebe211c
nikolina777 <152617209+nikolina777@users.noreply.github.com>
2024-03-23 18:40:51 +0100

    pokušaj auth

--- src/App.py
+++ src/App.py
@@ -15,34 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Model za prijavu
-class UserLogin(BaseModel):
-    username: str
-    password: str
-
-# Simulacija baze podataka
-fake_db = {}
-
-# Endpoint za prijavu
-@app.post("/login")
-async def login(user: UserLogin):
-    # Ovdje bi se obavila provjera korisničkih podataka u vašoj bazi podataka
-    # Na primjer, možete provjeriti korisničko ime i lozinku u MongoDB bazi
-    # Za ovaj primjer koristimo samo simulirani skup podataka
-    if user.username in fake_db and fake_db[user.username] == user.password:
-        return JSONResponse(content={"message": "Login successful"})
-    else:
-        return JSONResponse(status_code=401, content={"message": "Invalid username or password"})
-
-# Endpoint za registraciju
-@app.post("/signup")
-async def signup(user: UserLogin):
-    # Ovdje bi se obavila logika za registraciju korisnika
-    # Na primjer, možete dodati novog korisnika u MongoDB bazu
-    # Za ovaj primjer koristimo samo simulirani skup podataka
-    fake_db[user.username] = user.password
-    return JSONResponse(content={"message": "Signup successful"})
-
 # Endpoint za homeview stranicu
 @app.get("/home")
 async def home():
