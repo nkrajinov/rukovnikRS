6e69fc8bec305eec35d0a41dd23c234f9ca0c355
nikolina777 <152617209+nikolina777@users.noreply.github.com>
2024-04-03 10:23:08 +0200

    uvoz Note iz models.py u editnote

--- BACKEND/EditNote.py
+++ BACKEND/EditNote.py
@@ -5,6 +5,7 @@ from typing import Optional
 from auth import get_current_user
 from bson import ObjectId  # Dodali smo uvoz za pretvaranje stringa u ObjectId
 from bson.json_util import dumps
+from models import Note
 
 router = APIRouter()
 
@@ -17,19 +18,13 @@ db = client["rukovnikbaza"]
 # Odabir kolekcije (tablice)
 collection = db["kartice"]
 
-class Note(BaseModel):
-    grad: str
-    naziv_biljeske: str
-    tekst: str
-    user_id: Optional[str] = None  # Dodan user_id
-
 @router.post("/notes/")
 async def create_note(note: Note, user_id: str = Depends(get_current_user)):
     try:
         # Dodajte user_id u bilješku prije nego što je spremite
         note.user_id = user_id
         # Uklonite _id iz rezultata koji se sprema u bazu
-        note_dict = note.model_dump()
+        note_dict = note.dict()
         note_dict.pop("_id", None)
         result = collection.insert_one(note_dict)
         return {"message": "Note created successfully", "note_id": str(result.inserted_id)}
@@ -57,7 +52,7 @@ async def update_note(note_id: str, note: Note):
         object_id = ObjectId(note_id)
         
         # Koristite replace_one metodu kako biste zamijenili postojeći dokument s novim
-        result = collection.replace_one({"_id": object_id}, note.model_dump(by_alias=True))  # Zamijenjeno note.dict() s note.model_dump()
+        result = collection.replace_one({"_id": object_id}, note.dict(by_alias=True))
         
         # Provjerite je li dokument uspješno ažuriran
         if result.modified_count:
